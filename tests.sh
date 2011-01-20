525bbfdb4f0c40f0f32ea3397b3b948de1c7e1e4
Victor Engmark <victor.engmark@gmail.com>
2011-01-20 18:33:56 +0100

    Test for existence of file in output

--- tests.sh
+++ tests.sh
@@ -2,26 +2,35 @@
 #
 #
 
+#set -o errexit # Enable when shunit works with it
+set -o nounset
+
+declare -r directory="$(dirname -- "$(readlink -f -- "$0")")"
+
+# Command to test
+declare -r cmd="${directory}/fgit.sh"
+
+# Weird filename
+declare -r test_filename=$'--$`\! *@ \a\b\e\E\f\r\t\v\\\"\' \n'
+
+# Repositories
+declare -a repos_dirs=( foo bar )
+
+
 oneTimeSetUp()
 {
-    declare -r directory="$(dirname -- "$(readlink -f -- "$0")")"
+    test -x "$cmd" || exit 1
 
-    # Weird filename
-    declare -r test_filename=$'--$`\! *@ \a\b\e\E\f\r\t\v\\\"\' \n'
-
+    # Test directories and files
     test_dir="${__shunit_tmpDir}/${test_filename}"
-    mkdir -- "${test_dir}" || exit 1
+    mkdir -- "$test_dir" || exit 1
+
     stdout_file="${test_dir}/stdout"
     stderr_file="${test_dir}/stderr"
 
-    # Command to test
-    cmd="${directory}/fgit.sh"
-    test -x "$cmd" || exit 1
-    
-    # Repositories
     repos_parent="${test_dir}/repos"
-    repos_dirs='foo bar'
-    for repos_dir in $repos_dirs
+
+    for repos_dir in "${repos_dirs[@]}"
     do
         git init -- "${repos_parent}/${repos_dir}" || exit 1
     done
@@ -36,12 +45,41 @@ tearDown()
 
 test_status()
 {
-    "${cmd}" status -- "${repos_parent}" > >(tee "${stdout_file}") 2> >(tee "${stderr_file}" >&2)
+    "$cmd" status -- "$repos_parent" \
+        > >(tee "$stdout_file") 2> >(tee "$stderr_file" >&2)
     exit_code=$?
     assertEquals 'Wrong exit code' 0 $exit_code
-    assertNotNull 'Expected output to stdout' "$(cat "${stdout_file}")"
-    assertNull 'Unexpected output to stderr' "$(cat "${stderr_file}")"
+    assertNotNull 'Expected output to stdout' "$(cat "$stdout_file")"
+    assertNull 'Unexpected output to stderr' "$(cat "$stderr_file")"
 }
+
+
+test_parameter()
+{
+    "$cmd" status --short  -- "$repos_parent" \
+        > >(tee "$stdout_file") 2> >(tee "$stderr_file" >&2)
+    exit_code=$?
+    assertEquals 'Wrong exit code' 0 $exit_code
+    assertNotNull 'Expected output to stdout' "$(cat "$stdout_file")"
+    assertNull 'Unexpected output to stderr' "$(cat "$stderr_file")"
+}
+
+
+test_file_found()
+{
+    filename='foobar'
+    touch -- "${repos_parent}/${repos_dirs[0]}/${filename}"
+    "$cmd" status --short  -- "$repos_parent" \
+        > >(tee "$stdout_file") 2> >(tee "$stderr_file" >&2)
+    exit_code=$?
+    assertEquals 'Wrong exit code' 0 $exit_code
+    assertNotNull 'Expected output to stdout' "$(cat "$stdout_file")"
+    assertNull 'Unexpected output to stderr' "$(cat "$stderr_file")"
+
+    assertEquals "Should find $filename" "$filename" \
+        "$(grep -oe "$filename" -- "$stdout_file")"
+}
+
 
 # load and run shunit-ng
 test -n "${ZSH_VERSION:-}" && SHUNIT_PARENT=$0
