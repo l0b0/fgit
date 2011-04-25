a54b9f6a89f29c8f33cfd0afd93449984f6eaa50
Victor Engmark <victor.engmark@gmail.com>
2011-04-25 18:47:57 +0200

    Works with the new parameter structure

--- tests.sh
+++ tests.sh
@@ -13,9 +13,7 @@ declare -r cmd="${directory}/fgit.sh"
 # Weird filename
 declare -r test_filename=$'--$`\! *@ \a\b\e\E\f\r\t\v\\\"\' \n'
 
-# Repositories
-declare -a repos_dirs=( foo bar )
-
+declare -a repos_dirs
 
 oneTimeSetUp()
 {
@@ -29,41 +27,39 @@ oneTimeSetUp()
     stderr_file="${test_dir}/stderr"
 
     repos_parent="${test_dir}/repos"
-
-    for repos_dir in "${repos_dirs[@]}"
+    repos_names=( foo bar "$test_filename" )
+    for repos_name in "${repos_names[@]}"
     do
-        git init -- "${repos_parent}/${repos_dir}" || exit 1
+        repos_dir="${repos_parent}/${repos_name}"
+        repos_dirs+=( "$repos_dir" )
+        git init -- "${repos_dir}" || exit 1
     done
 }
 
-
 test_status()
 {
-    "$cmd" status -- "$repos_parent" \
-        > "$stdout_file" 2> "$stderr_file"
+    "$cmd" status -- "$repos_parent"/* > "$stdout_file" 2> "$stderr_file"
     exit_code=$?
     assertEquals 'Wrong exit code' 0 $exit_code
     assertNotNull 'Expected output to stdout' "$(cat "$stdout_file")"
     assertNull 'Unexpected output to stderr' "$(cat "$stderr_file")"
 }
-
 
 test_parameter()
 {
-    "$cmd" status --short  -- "$repos_parent" \
+    "$cmd" status --short  -- "$repos_parent"/* \
         > "$stdout_file" 2> "$stderr_file"
     exit_code=$?
     assertEquals 'Wrong exit code' 0 $exit_code
     assertNotNull 'Expected output to stdout' "$(cat "$stdout_file")"
     assertNull 'Unexpected output to stderr' "$(cat "$stderr_file")"
 }
-
 
 test_file_found()
 {
     filename='foobar'
-    touch -- "${repos_parent}/${repos_dirs[0]}/${filename}"
-    "$cmd" status --short  -- "$repos_parent" \
+    touch -- "${repos_dirs[0]}/${filename}"
+    "$cmd" status --short  -- "$repos_parent"/* \
         > "$stdout_file" 2> "$stderr_file"
     exit_code=$?
     assertEquals 'Wrong exit code' 0 $exit_code
@@ -73,7 +69,6 @@ test_file_found()
     assertEquals "Should find $filename" "$filename" \
         "$(grep -oe "$filename" -- "$stdout_file")"
 }
-
 
 # load and run shunit-ng
 test -n "${ZSH_VERSION:-}" && SHUNIT_PARENT=$0
