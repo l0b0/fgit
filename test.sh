a5e55b5a15f4d6728947f03ba864047ea7bcd27e
Victor Engmark <victor.engmark@gmail.com>
2012-05-14 20:41:13 +0200

    Standard file header

--- test.sh
+++ test.sh
@@ -1,6 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# Test fgit.sh
+# NAME
+#    test.sh - Test fgit.sh script
+#
+# BUGS
+#    https://github.com/l0b0/fgit/issues
+#
+# COPYRIGHT AND LICENSE
+#    Copyright (C) 2011-2012 Victor Engmark
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
 
 #set -o errexit # Enable when shunit works with it
 set -o nounset
